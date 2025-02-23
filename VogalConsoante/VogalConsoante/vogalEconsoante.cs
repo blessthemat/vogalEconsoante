fb7ee3173d34fe30f7653d2231e9df5984572b21
Matheus <matheus.viee.47@gmail.com>
2025-02-23 16:32:28 -0300

    Saber se é vogal ou consoante

---
+++ VogalConsoante/VogalConsoante/vogalEconsoante.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VogalEconsoante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma letra:");
+            string letra = Console.ReadLine();
+
+            if (letra == "a" ||
+                letra == "e" ||
+                letra == "i" ||
+                letra == "o" ||
+                letra == "u")
+            {
+                Console.WriteLine("A letra digitada é uma volgal.");
+            }
+            else
+            {
+                Console.WriteLine("A letra digitada é uma consoante.");
+            }
+
+
+        }
+    }
+}
